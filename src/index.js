6d4429ac91b552a2ef92e219bce162ea6f7605cd
Riad Benguella <benguella@gmail.com>
2016-10-08 23:40:45 +0100

    fix middleware, controls were not successfully initialized

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import is from './utils/is'
-import { createControls } from './controls'
+import createControls from './controls'
 import { create, asyncControls, wrapControls } from 'rungen'
 export * from './utils/helpers'
 
 const middleware = store => {
-  const runtime = create(
+  const runtime = create([
     ...asyncControls,
     ...wrapControls,
     ...createControls(store.dispatch, store.getState)
-  )
+  ])
+  
   return next => action =>
     is.iterator(action) ?
       runtime(action) :
